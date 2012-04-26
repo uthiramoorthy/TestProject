dfbfd7544d3882eee3f20b993430494e14b3bb82
Uthiramoorthy M <uthiramoorthy@sourcebits.com>
2012-04-26 11:10:20 +0530

    test

--- src/com/uthira/test/TestProjectActivity.java
+++ src/com/uthira/test/TestProjectActivity.java
@@ -9,5 +9,7 @@ public class TestProjectActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // Eclipse commit
     }
 }
